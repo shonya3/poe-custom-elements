b3e72c2c518985e707ca5957691f890cd4c98d0c
shonya3 <poeshonya3@gmail.com>
2024-04-27 15:04:54 +0300

    determine header image

--- src/elements/item-info/poe-item-info-header.ts
+++ src/elements/item-info/poe-item-info-header.ts
@@ -1,6 +1,7 @@
 import { LitElement, html, css, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import type { PoeItem } from '../../poe.types';
+import { classMap } from 'lit/directives/class-map.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -13,7 +14,25 @@ export class ItemInfoHeader extends LitElement {
 	@property({ type: Object }) item!: PoeItem;
 
 	protected render(): TemplateResult {
-		return html`<header>Header</header>`;
+		const frame = frameKind(this.item.frameType);
+		const size = frame ? singleOrDouble(frame, this.item.identified) : null;
+
+		return html`<header
+			class=${classMap({
+				header: true,
+				'header--single': size === 'single',
+				'header--double': size === 'double',
+				'header--necropolis': frame === 'necropolis',
+			})}
+			style="background: ${headerBackgroundUrl(frame ?? 'normal', this.item.identified)}"
+		>
+			${size === 'double'
+				? html`
+						<div class="content">${this.item.name}</div>
+						<div class="content">${this.item.baseType}</div>
+				  `
+				: html`<div class="content">${this.item.baseType}</div>`}
+		</header>`;
 	}
 
 	static styles = css`
@@ -22,5 +41,96 @@ export class ItemInfoHeader extends LitElement {
 			margin: 0;
 			box-sizing: border-box;
 		}
+
+		.header {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			flex-direction: column;
+		}
+
+		.header--single {
+			height: 27px;
+		}
+
+		.header--double {
+			height: 54px;
+		}
+
+		.header--necropolis {
+			height: 45px;
+		}
+
+		.content {
+			color: #fff;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100%;
+		}
 	`;
+}
+
+type FrameKind = 'normal' | 'magic' | 'rare' | 'unique' | 'gem' | 'currency' | 'necropolis';
+
+function headerBackgroundUrl(frameKind: FrameKind, identified?: boolean): string {
+	const size = singleOrDouble(frameKind, identified);
+	const left = `url(${headerAssetUrl(frameKind, size, 'left')}) top left no-repeat`;
+	const right = `url(${headerAssetUrl(frameKind, size, 'right')}) top right no-repeat`;
+	const middle = `url(${headerAssetUrl(frameKind, size, 'middle')}) top center`;
+	return `${left},${right},${middle}`;
+}
+
+function headerAssetUrl(
+	frameKind: FrameKind,
+	size: 'single' | 'double',
+	side: 'left' | 'middle' | 'right',
+	url = '/poe-images/'
+): string {
+	return `${url}${['header', size === 'double' ? 'double' : '', frameKind, side]
+		.filter(s => s.length > 0)
+		.join('-')}.png`;
+}
+
+function singleOrDouble(frameKind: FrameKind, identified?: boolean): 'single' | 'double' {
+	switch (frameKind) {
+		case 'normal':
+			return 'single';
+		case 'magic':
+			return 'single';
+		case 'rare':
+			return identified ? 'double' : 'single';
+		case 'unique':
+			return identified ? 'double' : 'single';
+		case 'gem':
+			return 'single';
+		case 'currency':
+			return 'single';
+		case 'necropolis':
+			return 'single';
+	}
+}
+
+/** https://www.pathofexile.com/developer/docs/reference#type-FrameType */
+function frameKind(frameType: number): FrameKind | null {
+	switch (frameType) {
+		case 0:
+			return 'normal';
+		case 1:
+			return 'magic';
+		case 2:
+			return 'rare';
+		case 3:
+			return 'unique';
+		case 4:
+			return 'gem';
+		case 5:
+			return 'currency';
+		// case 6:
+		// return 'divination card';
+		case 11:
+			return 'necropolis';
+		default:
+			return null;
+	}
 }
--- src/elements/item-info/poe-item-info.ts
+++ src/elements/item-info/poe-item-info.ts
@@ -1,7 +1,6 @@
 import { LitElement, html, css, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import type { PoeItem } from '../../poe.types';
-import { classMap } from 'lit/directives/class-map.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -15,20 +14,11 @@ export class PoeItemInfoElement extends LitElement {
 	@property({ type: Object }) item!: PoeItem;
 
 	protected render(): TemplateResult {
-		const typeHead = typeHeader(this.item);
-
 		if (!this.item) {
 			return html`<p style="color: red">No Poe Api item data (.item)</p>`;
 		}
 
-		return html`<header
-			class=${classMap({
-				header: true,
-				'header--single': typeHead === 'single',
-				'header--double': typeHead === 'double',
-			})}
-			style="background: ${headerBackgroundUrl(this.item)}"
-		></header>`;
+		return html`<poe-item-info-header .item=${this.item}></poe-item-info-header>`;
 	}
 
 	static styles = css`
@@ -38,55 +28,7 @@ export class PoeItemInfoElement extends LitElement {
 			box-sizing: border-box;
 		}
 		:host {
-			--header-single-height: 27px;
 			display: block;
-		}
-
-		.header {
-			background-color: red;
-		}
-
-		.header--single {
-			height: var(--header-single-height);
-		}
-
-		.header--double {
-			height: calc(2 * var(--header-single-height));
 		}
 	`;
 }
-
-function headerAssetUrl(
-	size: 'single' | 'double',
-	rarity: string,
-	side: 'left' | 'middle' | 'right',
-	url = '/poe-images/'
-): string {
-	return `${url}header-${size === 'double' ? 'double' : ''}-${rarity.toLowerCase()}-${side}.png`;
-}
-
-function headerBackgroundUrl(item: PoeItem): string {
-	const left = `url(${headerAssetUrl(typeHeader(item), item.rarity, 'left')}) top left no-repeat`;
-	const right = `url(${headerAssetUrl(typeHeader(item), item.rarity, 'right')}) top right no-repeat`;
-	const mid = `url(${headerAssetUrl(typeHeader(item), item.rarity, 'middle')}) top center`;
-	return `${left},${right},${mid}`;
-}
-
-function typeHeader(item: PoeItem): 'single' | 'double' {
-	switch (item.frameType) {
-		case 0:
-			return 'single';
-		case 1:
-			return 'single';
-		case 2:
-			return 'double';
-		case 3:
-			return 'double';
-		case 9:
-			return 'double';
-		default:
-			throw new Error(`Unexpected item frameType: ${item.frameType}`);
-	}
-}
-
-// necropolis 45px
