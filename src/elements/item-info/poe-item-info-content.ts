ba435e291cee4d3688d3d1c6ea3464f6dcf24b58
shonya3 <poeshonya3@gmail.com>
2024-04-30 12:55:51 +0300

    set size

--- src/elements/item-info/poe-item-info-content.ts
+++ src/elements/item-info/poe-item-info-content.ts
@@ -75,6 +75,8 @@ export class PoeItemInfoContentElement extends LitElement {
 			font-family: fontin;
 			display: inline-block;
 			background-color: rgba(0, 0, 0, 0.8);
+			width: 100%;
+			height: 100%;
 		}
 
 		.content {
