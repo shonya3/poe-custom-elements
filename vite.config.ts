37648df8f7e81cc0e82caf90477fbb6ac9540313
shonya3 <poeshonya3@gmail.com>
2024-05-09 12:17:17 +0300

    add build config

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+	build: {
+		lib: {
+			formats: ['es'],
+			// Could also be a dictionary or array of multiple entry points
+			entry: [
+				resolve(import.meta.dirname, 'src/elements/poe-item.ts'),
+				resolve(import.meta.dirname, 'src/elements/poe-stash-tab.ts'),
+			],
+		},
+		rollupOptions: {},
+	},
+	plugins: [dts()],
+});
