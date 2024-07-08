a7040ffb8bccb752303879279efc8b4aae7bfa0d
shonya3 <poeshonya3@gmail.com>
2024-07-08 22:54:10 +0300

    update cem config

--- custom-elements-manifest.config.js
+++ custom-elements-manifest.config.js
@@ -1,5 +1,6 @@
 /** https://custom-elements-manifest.open-wc.org/analyzer/config/ */
 export default {
-	exclude: ['jsons', 'src/app-root.ts', 'src/**/*.styles.ts'],
+	exclude: ['src/app-root.ts', 'src/**/*.styles.ts'],
 	litelement: true,
+	globs: ['src/**/*.ts'],
 };
