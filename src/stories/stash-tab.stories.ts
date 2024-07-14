ebb8d7498b41cb8fdd8e594b756eea637afcb751
shonya3 <poeshonya3@gmail.com>
2024-07-14 19:19:09 +0300

    add chromatic and add divination tab to story

--- src/stories/stash-tab.stories.ts
+++ src/stories/stash-tab.stories.ts
@@ -3,6 +3,7 @@ import { PoeStashTabElement } from '../elements/poe-stash-tab';
 import { html } from 'lit';
 import '../elements/poe-stash-tab';
 import { quadStd } from '../../jsons/tabs/quadStd';
+import { divination } from '../../jsons/tabs/divination';
 
 export default {
 	tags: ['autodocs'],
@@ -13,5 +14,11 @@ export default {
 export const Quad = {
 	render() {
 		return html`<poe-stash-tab .tab=${quadStd}></poe-stash-tab>`;
+	},
+};
+
+export const Divination = {
+	render() {
+		return html`<poe-stash-tab .tab=${divination}></poe-stash-tab>`;
 	},
 };
