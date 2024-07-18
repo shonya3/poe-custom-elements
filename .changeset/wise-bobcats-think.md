ecc4298c51a21198f8dcfe882790b1d1b429e8d9
shonya3 <poeshonya3@gmail.com>
2024-07-19 00:29:57 +0300

    add poe-item-card story

---
+++ .changeset/wise-bobcats-think.md
@@ -0,0 +1,5 @@
+---
+"poe-custom-elements": patch
+---
+
+item-card: fix icon url
---
+++ src/stories/item-card.stories.ts
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+import '../elements/poe-item-card';
+import { PoeItemCardElement } from '../elements/poe-item-card';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+export default {
+	tags: ['autodocs'],
+	title: 'Components/poe-item-card',
+	component: 'poe-item-card',
+	decorators: [story => html`<div style="height: 30rem">${story()}</div>`],
+	args: {
+		name: 'Rain of Chaos',
+	},
+	render: args =>
+		html`<poe-item-card
+			name=${args.name}
+			stack-size=${ifDefined(args.stackSize)}
+			max-stack-size=${ifDefined(args.maxStackSize)}
+		></poe-item-card>`,
+} satisfies Meta<PoeItemCardElement>;
+type Story = StoryObj<PoeItemCardElement>;
+
+/**
+ * You need to manually set max-stack-size -
+ * element does not know about it.
+ */
+export const StackMaxed = {
+	args: {
+		stackSize: 8,
+		maxStackSize: 8,
+	},
+} satisfies Story;
+export const Default = {};
+export const Stacksize = {
+	args: {
+		stackSize: 4,
+	},
+} satisfies Story;
