b100eed6af230778565eae5dd78a239018312fa7
shonya3 <poeshonya3@gmail.com>
2024-07-22 22:42:16 +0300

    story: add stories for supported stashtabs

--- src/stories/stash-tab.stories.ts
+++ src/stories/stash-tab.stories.ts
@@ -4,21 +4,41 @@ import { html } from 'lit';
 import '../elements/poe-stash-tab';
 import { quadStd } from '../../jsons/tabs/quadStd';
 import { divination } from '../../jsons/tabs/divination';
+import { TabWithItems } from '../poe.types';
+import premium from '../../jsons/PremiumStash.json';
+import { essence } from '../../jsons/tabs/essence';
+import { currencyTab } from '../../jsons/tabs/currencyTab';
+import { blight } from '../../jsons/tabs/blight';
+import { fragments } from '../../jsons/tabs/myfragments';
 
 export default {
 	tags: ['autodocs'],
 	title: 'Components/poe-stash-tab',
 	component: 'poe-stash-tab',
+	render: (args: { tab: TabWithItems }) => html`<poe-stash-tab .tab=${args.tab}></poe-stash-tab>`,
 } satisfies Meta<PoeStashTabElement>;
 
 export const Quad = {
-	render() {
-		return html`<poe-stash-tab .tab=${quadStd}></poe-stash-tab>`;
-	},
+	args: { tab: quadStd },
 };
-
+export const Premium = {
+	args: { tab: premium },
+};
+export const Essence = {
+	args: { tab: essence },
+};
+export const Currency = {
+	args: { tab: currencyTab },
+};
+export const Blight = {
+	args: { tab: blight },
+};
+export const Fragment = {
+	args: { tab: fragments },
+};
 export const Divination = {
-	render() {
-		return html`<poe-stash-tab .tab=${divination}></poe-stash-tab>`;
-	},
+	args: { tab: divination },
+};
+export const Normal = {
+	args: { tab: premium },
 };
