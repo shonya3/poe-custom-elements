bac70cb09ad35a40f1ceac8e91632199540b8d80
shonya3 <poeshonya3@gmail.com>
2024-10-22 11:37:00 +0300

    feat: Prevent outer scrolling when the arrow keys are pressed while focus is inside the stash tab.

---
+++ .changeset/proud-actors-battle.md
@@ -0,0 +1,5 @@
+---
+"poe-custom-elements": patch
+---
+
+Prevent outer scrolling when the arrow keys are pressed while focus is inside the stash tab.
--- src/elements/poe-stash-tab.ts
+++ src/elements/poe-stash-tab.ts
@@ -28,21 +28,21 @@ export class PoeStashTabElement extends LitElement {
 	/** PoE API tab data https://www.pathofexile.com/developer/docs/reference#stashes-get */
 	@property({ type: Object }) tab!: TabWithItems;
 	/** The state of search input for DivinationStashType */
-	@state() searchDivinationCardsQuery = '';
+	@state() search_query_for_divination_stash = '';
 	/** Mutable clone of tab */
 	#tab!: TabWithItems;
-	get #hasFocusWithin(): boolean {
+	get #has_focus_within(): boolean {
 		return this.matches(':focus-within');
 	}
-	get #activeItemElement(): PoeItemElement | null {
+	get #active_item_element(): PoeItemElement | null {
 		return this.shadowRoot?.querySelector('poe-item:focus') ?? null;
 	}
 
 	protected willUpdate(map: PropertyValueMap<this>): void {
 		if (map.has('tab') && this.tab) {
 			this.#tab = structuredClone(this.tab);
-			const cells = cellsSideCount(this.#tab.type);
-			adjustItemXYforCustomTab(this.#tab, cells);
+			const cells = get_stash_tab_side_cell_count(this.#tab.type);
+			adjust_item_x_y_for_custom_tab(this.#tab, cells);
 			this.#tab.items = orderItems(this.#tab.items);
 			this.style.setProperty('--cells-side-count', cells.toString());
 			this.style.setProperty('--background-image', `url(${tabImageSrc(this.#tab.type)})`);
@@ -80,7 +80,7 @@ export class PoeStashTabElement extends LitElement {
 								class=${classMap({
 									'item--visible': item.baseType
 										.toLowerCase()
-										.includes(this.searchDivinationCardsQuery),
+										.includes(this.search_query_for_divination_stash),
 								})}
 							></poe-item>
 						</li>`
@@ -92,7 +92,7 @@ export class PoeStashTabElement extends LitElement {
 						autocomplete="off"
 						id="search-divination-stash-tab"
 						type="text"
-						.value=${this.searchDivinationCardsQuery}
+						.value=${this.search_query_for_divination_stash}
 						@input=${this.#handleDivinationCardsQueryInput}
 					/>`
 				: null}
@@ -101,41 +101,50 @@ export class PoeStashTabElement extends LitElement {
 
 	#handleDivinationCardsQueryInput(e: InputEvent) {
 		if (e.target instanceof HTMLInputElement) {
-			this.searchDivinationCardsQuery = e.target.value.trim().toLowerCase();
+			this.search_query_for_divination_stash = e.target.value.trim().toLowerCase();
 		}
 	}
 
 	connectedCallback(): void {
 		super.connectedCallback();
 		appendFontinStyle();
-		window.addEventListener('keydown', this.#navigateItemsWithArrowKeys);
+		window.addEventListener('keydown', this.#navigate_items_with_arrow_keys);
 	}
 
 	disconnectedCallback(): void {
 		super.disconnectedCallback();
-		window.removeEventListener('keydown', this.#navigateItemsWithArrowKeys);
+		window.removeEventListener('keydown', this.#navigate_items_with_arrow_keys);
 	}
 
-	#navigateItemsWithArrowKeys = async (e: KeyboardEvent): Promise<void> => {
-		if (this.#hasFocusWithin) {
-			if (['ArrowDown', 'ArrowRight', 'ArrowUp', 'ArrowLeft'].includes(e.code)) {
-				const direction = e.code.slice(5).toLowerCase() as Direction;
-				const activeItemElement = this.#activeItemElement;
-				if (activeItemElement) {
-					const item = await findClosestItem({
-						activeItem: activeItemElement.item,
-						direction,
-						items: this.#tab.items,
-						tabElement: this,
-						tabCellsSideCount: cellsSideCount(this.#tab.type),
-					});
-
-					if (item) {
-						item.focus();
-					}
-				}
-			}
+	#navigate_items_with_arrow_keys = async (e: KeyboardEvent): Promise<void> => {
+		if (!this.#has_focus_within) {
+			return;
 		}
+
+		if (!['ArrowDown', 'ArrowRight', 'ArrowUp', 'ArrowLeft'].includes(e.code)) {
+			return;
+		}
+
+		const active_item_element = this.#active_item_element;
+		if (!active_item_element) {
+			return;
+		}
+
+		const direction = e.code.slice(5).toLowerCase() as Direction;
+		const item = await find_closest_item_element({
+			active_item: active_item_element.item,
+			direction,
+			items: this.#tab.items,
+			stash_tab_element: this,
+			tab_cells_side_count: get_stash_tab_side_cell_count(this.#tab.type),
+		});
+		if (!item) {
+			return;
+		}
+
+		item.focus();
+		// Prevent outer scrolling when the arrow keys are pressed while focus is inside the stash tab.
+		e.preventDefault();
 	};
 
 	static styles = css`
@@ -199,25 +208,32 @@ export class PoeStashTabElement extends LitElement {
 	`;
 }
 
-function adjustItemXYforCustomTab(tab: TabWithItems, cellsSideCount: number): void {
-	if (!cellsSideCount) {
+/**
+ * Adjusts the (x, y) coordinates of items in stash tabs that have a custom, non-grid layout,
+ * rearranging them to fit into a grid layout.
+ *
+ * @param tab - The stash tab containing the items.
+ * @param cells_side_count - The number of cells on one side of the grid layout.
+ */
+function adjust_item_x_y_for_custom_tab(tab: TabWithItems, cells_side_count: number): void {
+	if (!cells_side_count) {
 		return;
 	}
 	if (tab.type === 'FragmentStash') {
-		let currentXForY1Group = 0;
+		let current_x_for_y1_group = 0;
 		const STARTING_Y_FOR_Y1_GROUP = 12;
 		const Y_OFFSET_FOR_Y2_GROUP = 13;
 		tab!.items.forEach(item => {
 			switch (item.y) {
 				case 0: {
-					item.y = Math.floor(item.x / cellsSideCount);
-					item.x = item.x % cellsSideCount;
+					item.y = Math.floor(item.x / cells_side_count);
+					item.x = item.x % cells_side_count;
 					break;
 				}
 				case 1: {
-					item.y = STARTING_Y_FOR_Y1_GROUP + Math.floor(currentXForY1Group / cellsSideCount);
-					item.x = currentXForY1Group % cellsSideCount;
-					currentXForY1Group++;
+					item.y = STARTING_Y_FOR_Y1_GROUP + Math.floor(current_x_for_y1_group / cells_side_count);
+					item.x = current_x_for_y1_group % cells_side_count;
+					current_x_for_y1_group++;
 					break;
 				}
 				case 2: {
@@ -244,14 +260,20 @@ function adjustItemXYforCustomTab(tab: TabWithItems, cellsSideCount: number): vo
 		}
 
 		tab!.items.forEach(item => {
-			item.y = Math.floor(item.x / cellsSideCount);
-			item.x = item.x % cellsSideCount;
+			item.y = Math.floor(item.x / cells_side_count);
+			item.x = item.x % cells_side_count;
 		});
 	}
 }
 
-function cellsSideCount(stashType: StashType): number {
-	switch (stashType) {
+/**
+ * Returns the number of cells on one side of the stash tab based on the stash type.
+ *
+ * @param stash_type - The type of the stash tab (e.g., PremiumStash, QuadStash, etc.).
+ * @returns The number of cells on one side of the stash tab.
+ */
+function get_stash_tab_side_cell_count(stash_type: StashType): number {
+	switch (stash_type) {
 		case 'PremiumStash':
 		case 'NormalStash':
 		case 'BlightStash':
@@ -265,8 +287,8 @@ function cellsSideCount(stashType: StashType): number {
 	}
 }
 
-function tabImageSrc(stashType: StashType): string {
-	switch (stashType) {
+function tabImageSrc(stash_type: StashType): string {
+	switch (stash_type) {
 		case 'PremiumStash':
 		case 'NormalStash':
 		case 'EssenceStash':
@@ -282,8 +304,8 @@ function tabImageSrc(stashType: StashType): string {
 	}
 }
 
-function sizeOfCellPixels(stashType: StashType): `${string}px` {
-	return `${564 / cellsSideCount(stashType)}px`;
+function sizeOfCellPixels(stash_type: StashType): `${number}px` {
+	return `${564 / get_stash_tab_side_cell_count(stash_type)}px`;
 }
 
 function orderItems(items: Array<PoeItem>): Array<PoeItem> {
@@ -293,33 +315,36 @@ function orderItems(items: Array<PoeItem>): Array<PoeItem> {
 	});
 }
 
-async function findClosestItem({
-	activeItem,
+/**
+ * Finds the item element closest to the active item based on the provided direction.
+ */
+async function find_closest_item_element({
+	active_item,
 	direction,
 	items,
-	tabElement,
-	tabCellsSideCount,
+	stash_tab_element,
+	tab_cells_side_count,
 }: {
-	activeItem: PoeItem;
+	active_item: PoeItem;
 	direction: Direction;
 	items: Array<PoeItem>;
-	tabElement: PoeStashTabElement;
-	tabCellsSideCount: number;
-}) {
+	stash_tab_element: PoeStashTabElement;
+	tab_cells_side_count: number;
+}): Promise<PoeItemElement | null> {
 	if (direction === 'down') {
-		let currentX = activeItem.x;
-		let currentY = activeItem.y + activeItem.h;
+		let current_x = active_item.x;
+		let current_y = active_item.y + active_item.h;
 		let item = null;
 
-		if (currentY + 1 > tabCellsSideCount) {
+		if (current_y + 1 > tab_cells_side_count) {
 			return null;
 		}
 		while (!item) {
-			for (let dx = 0; dx < activeItem.w; dx++) {
-				item = await itemElementFromXY({
-					tabElement,
+			for (let dx = 0; dx < active_item.w; dx++) {
+				item = await item_element_from_x_y({
+					stash_tab_element,
 					items,
-					coordinates: { x: currentX + dx, y: currentY },
+					coordinates: { x: current_x + dx, y: current_y },
 				});
 
 				if (item) {
@@ -327,33 +352,33 @@ async function findClosestItem({
 				}
 			}
 
-			if (currentY === tabCellsSideCount - 1 && currentX === tabCellsSideCount - 1) {
+			if (current_y === tab_cells_side_count - 1 && current_x === tab_cells_side_count - 1) {
 				break;
 			}
 
-			if (currentY === tabCellsSideCount - 1) {
-				currentY = activeItem.y;
-				currentX++;
+			if (current_y === tab_cells_side_count - 1) {
+				current_y = active_item.y;
+				current_x++;
 			} else {
-				currentY++;
+				current_y++;
 			}
 		}
 
 		return item;
 	} else if (direction === 'right') {
-		let currentX = activeItem.x + activeItem.w;
-		let currentY = activeItem.y;
+		let current_x = active_item.x + active_item.w;
+		let current_y = active_item.y;
 		let item = null;
 
-		if (currentX + 1 > tabCellsSideCount) {
+		if (current_x + 1 > tab_cells_side_count) {
 			return null;
 		}
 		while (!item) {
-			for (let dy = 0; dy < activeItem.h; dy++) {
-				item = await itemElementFromXY({
-					tabElement,
+			for (let dy = 0; dy < active_item.h; dy++) {
+				item = await item_element_from_x_y({
+					stash_tab_element,
 					items,
-					coordinates: { x: currentX, y: currentY + dy },
+					coordinates: { x: current_x, y: current_y + dy },
 				});
 
 				if (item) {
@@ -361,33 +386,33 @@ async function findClosestItem({
 				}
 			}
 
-			if (currentY === tabCellsSideCount - 1 && currentX === tabCellsSideCount - 1) {
+			if (current_y === tab_cells_side_count - 1 && current_x === tab_cells_side_count - 1) {
 				break;
 			}
 
-			if (currentX === tabCellsSideCount - 1) {
-				currentX = activeItem.x + activeItem.w;
-				currentY++;
+			if (current_x === tab_cells_side_count - 1) {
+				current_x = active_item.x + active_item.w;
+				current_y++;
 			} else {
-				currentX++;
+				current_x++;
 			}
 		}
 
 		return item;
 	} else if (direction === 'up') {
-		let currentX = activeItem.x;
-		let currentY = activeItem.y - 1;
+		let current_x = active_item.x;
+		let current_y = active_item.y - 1;
 		let item = null;
 
-		if (currentY < 0) {
+		if (current_y < 0) {
 			return null;
 		}
 		while (!item) {
-			for (let dx = 0; dx < activeItem.w; dx++) {
-				item = await itemElementFromXY({
-					tabElement,
+			for (let dx = 0; dx < active_item.w; dx++) {
+				item = await item_element_from_x_y({
+					stash_tab_element,
 					items,
-					coordinates: { x: currentX + dx, y: currentY },
+					coordinates: { x: current_x + dx, y: current_y },
 				});
 
 				if (item) {
@@ -395,33 +420,33 @@ async function findClosestItem({
 				}
 			}
 
-			if (currentY === 0 && currentX === tabCellsSideCount - 1) {
+			if (current_y === 0 && current_x === tab_cells_side_count - 1) {
 				break;
 			}
 
-			if (currentY === 0) {
-				currentY = activeItem.y;
-				currentX = currentX + 1;
+			if (current_y === 0) {
+				current_y = active_item.y;
+				current_x = current_x + 1;
 			} else {
-				currentY--;
+				current_y--;
 			}
 		}
 
 		return item;
 	} else if (direction === 'left') {
-		let currentX = activeItem.x - 1;
-		let currentY = activeItem.y;
+		let current_x = active_item.x - 1;
+		let current_y = active_item.y;
 		let item = null;
 
-		if (currentX < 0) {
+		if (current_x < 0) {
 			return null;
 		}
 		while (!item) {
-			for (let dy = 0; dy < activeItem.h; dy++) {
-				item = await itemElementFromXY({
-					tabElement,
+			for (let dy = 0; dy < active_item.h; dy++) {
+				item = await item_element_from_x_y({
+					stash_tab_element,
 					items,
-					coordinates: { x: currentX, y: currentY + dy },
+					coordinates: { x: current_x, y: current_y + dy },
 				});
 
 				if (item) {
@@ -429,23 +454,25 @@ async function findClosestItem({
 				}
 			}
 
-			if (currentX === 0 && currentY === tabCellsSideCount - 1) {
+			if (current_x === 0 && current_y === tab_cells_side_count - 1) {
 				break;
 			}
 
-			if (currentX === 0) {
-				currentX = activeItem.x;
-				currentY = currentY + 1;
+			if (current_x === 0) {
+				current_x = active_item.x;
+				current_y = current_y + 1;
 			} else {
-				currentX--;
+				current_x--;
 			}
 		}
 
 		return item;
 	}
+
+	return null;
 }
 
-function itemFromXY({
+function item_from_x_y({
 	coordinates,
 	items,
 }: {
@@ -469,16 +496,16 @@ function itemFromXY({
 	return null;
 }
 
-async function itemElementFromXY({
-	tabElement,
+async function item_element_from_x_y({
+	stash_tab_element,
 	coordinates,
 	items,
 }: {
-	tabElement: PoeStashTabElement;
+	stash_tab_element: PoeStashTabElement;
 	coordinates: { x: number; y: number };
 	items: Array<PoeItem>;
 }): Promise<PoeItemElement | null> {
-	const item = itemFromXY({
+	const item = item_from_x_y({
 		coordinates,
 		items,
 	});
@@ -486,12 +513,14 @@ async function itemElementFromXY({
 		return null;
 	}
 
-	if (!tabElement.shadowRoot!.querySelector('poe-item')) {
-		await tabElement.updateComplete;
+	if (!stash_tab_element.shadowRoot!.querySelector('poe-item')) {
+		await stash_tab_element.updateComplete;
 	}
 
 	return (
-		tabElement.shadowRoot?.querySelector<PoeItemElement>(`poe-item[data-x="${item.x}"][data-y="${item.y}"]`) ?? null
+		stash_tab_element.shadowRoot?.querySelector<PoeItemElement>(
+			`poe-item[data-x="${item.x}"][data-y="${item.y}"]`
+		) ?? null
 	);
 }
 
