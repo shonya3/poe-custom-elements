f604a9170a4d7dcb7b32c7813b127ce51f581bcb
shonya3 <poeshonya3@gmail.com>
2026-07-24 21:02:03 +0300

    updat ecardElementData shape

--- src/elements/divination-card/data.ts
+++ src/elements/divination-card/data.ts
@@ -5,19 +5,21 @@ export const cardElementData: CardElementData[] = json;
  * https://github.com/shonya3/divicards/tree/main/crates/card_element
  */
 export type CardElementData = {
-	slug: string;
+  slug: string;
 	name: string;
 	artFilename: string;
 	flavourText: string;
 	stackSize: number | null;
 	rewardHtml: string;
-	dropLevel: DropLevel;
+	minLevel: number;
+	unique: UniqueReward | null;
 };
 
-export type DropLevel = {
-	level: { min: number | null; max: number | null };
-	label: string;
+export type UniqueReward = {
+	name: string;
+	item_class: string;
 };
+
 
 export function findCardBySlug(slug: string): CardElementData | null {
 	return cardElementData.find(card => card.slug === slug) ?? null;
